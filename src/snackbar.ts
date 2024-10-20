f48fb38ca9c03b9b34a2b82f8d4b0dd5b5849505
lingbopro <ilovemc@zohomail.cn>
2024-10-20 10:50:31 +0800

    snackbar icon slot
    add `icon` slot for snackbar to change icon
    
    Issue: #22

--- src/snackbar.ts
+++ src/snackbar.ts
@@ -8,7 +8,7 @@ const props = {
   duration: 4000
 }
 
-const style = /*css*/`
+const style = /*css*/ `
 :host{
   display: inline-block;
   vertical-align: middle;
@@ -66,11 +66,14 @@ const style = /*css*/`
 :host([type=error]) .error{
   display: block;
 }
-.icon{
+.icon,
+::slotted(svg[slot=icon]),
+::slotted(s-icon[slot=icon]){
   width: 24px;
   height: 24px;
   flex-shrink: 0;
   fill: currentColor;
+  color: currentColor;
   margin-left: 16px;
   margin-right: -4px;
 }
@@ -121,12 +124,14 @@ const template = /*html*/`
 <slot name="trigger"></slot>
 <div class="wrapper" part="wrapper">
   <div class="container" part="container">
-    <svg class="icon hint" viewBox="0 -960 960 960">
-      <path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"></path>
-    </svg>
-    <svg class="icon error" viewBox="0 -960 960 960">
-      <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"></path>
-    </svg>
+    <slot name="icon" part="icon">
+      <svg class="icon hint" viewBox="0 -960 960 960">
+        <path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"></path>
+      </svg>
+      <svg class="icon error" viewBox="0 -960 960 960">
+        <path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"></path>
+      </svg>
+    </slot>
     <div class="text" part="text">
       <slot></slot>
     </div>
@@ -140,6 +145,7 @@ const show = (options: string | {
   text: string
   type?: typeof props['type']
   duration?: number
+  icon?: string | Element
   action?: string | {
     text: string
     click: (event: MouseEvent) => unknown
@@ -158,6 +164,17 @@ const show = (options: string | {
     if (options.root) root = options.root
     snackbar.textContent = options.text
     snackbar.type = options.type ?? 'basic'
+    if (options.icon) {
+      const icon = document.createElement('s-icon')
+      icon.slot = 'icon'
+      if (typeof options.icon === 'string') {
+        icon.innerHTML = options.icon
+      }
+      else {
+        icon.appendChild(options.icon)
+      }
+      snackbar.appendChild(icon)
+    }
     if (options.action) {
       const action = document.createElement('s-button')
       action.type = 'text'

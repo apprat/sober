05a23d674237a1f3eee68ca5c56d693adaa1c083
apprat <apprat@qq.com>
2025-03-30 22:03:19 +0800

    动画过渡曲线优化

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -11,7 +11,7 @@ const style = /*css*/`
 :host{
   display: block;
   height: 16px;
-  animation: skeleton 1.4s ease infinite;
+  animation: skeleton var(--s-motion-duration-extra-long4, ${Theme.motionDurationExtraLong4}) var(--s-motion-easing-standard, ${Theme.motionEasingStandard}) infinite;
   background: linear-gradient(90deg, var(--s-color-surface-container-high, ${Theme.colorSurfaceContainerHigh}) 25%, var(--s-color-surface-container-highest, ${Theme.colorSurfaceContainerHighest}) 37%, var(--s-color-surface-container-high, ${Theme.colorSurfaceContainerHigh}) 63%);
   background-size: 400% 100%;
   border-radius: 8px;

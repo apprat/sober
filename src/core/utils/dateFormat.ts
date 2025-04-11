57b41691b389ac316fdc718a45c04723552a9630
apprat <apprat@qq.com>
2025-04-11 20:32:39 +0800

    更新格式

--- src/core/utils/dateFormat.ts
+++ src/core/utils/dateFormat.ts
@@ -1,7 +1,7 @@
-export const dateFormat = (date: Date | string, format = '%Y-%m-%d') => {
+export const dateFormat = (date: Date | string, format = 'yyyy-MM-dd') => {
   if (typeof date === 'string') date = new Date(date)
   const Y = date.getFullYear()
   const m = date.getMonth() + 1
   const d = date.getDate()
-  return format.replace(/%Y/g, Y.toString()).replace(/%m/g, m.toString().padStart(2, '0')).replace(/%d/g, d.toString().padStart(2, '0'))
+  return format.replace('yyyy', Y.toString()).replace('MM', m.toString().padStart(2, '0')).replace('DD', d.toString().padStart(2, '0'))
 }
